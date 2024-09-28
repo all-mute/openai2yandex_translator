d127bcb1220cc793a31c1cf1a0e4c4cd6caa8245
All Mute ✓ <davidmerkulov@yandex.ru>
2024-09-28 18:43:03 +0300

    healthcheck

--- main.py
+++ main.py
@@ -179,5 +179,9 @@ async def embeddings(request: Request):
         #print(e)
         pass
 
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug")
