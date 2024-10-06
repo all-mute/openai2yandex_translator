a94bfd6a191d3d922b27e74065c7521382a97da4
All Mute ✓ <davidmerkulov@yandex.ru>
2024-10-06 20:22:30 +0300

    logs

--- app/yandex.py
+++ app/yandex.py
@@ -1,24 +1,47 @@
 import requests
 import time
 from fastapi import HTTPException
+from loguru import logger
 
-# Генерация ответа от Yandex GPT
-def generate_yandexgpt_response(messages, model, temperature, max_tokens, yandex_api_key, folder_id) -> str:
-    # Функция для отправки запроса в Yandex GPT
-    def send_request_safety(payload, api_key):
+def generate_yandexgpt_response(messages, model: str, temperature: float, max_tokens: int, yandex_api_key: str, folder_id: str) -> str:
+    """
+    Генерация ответа от Yandex GPT.
+
+    :param messages: Список сообщений для генерации ответа.
+    :param model: Модель, используемая для генерации.
+    :param temperature: Температура для генерации.
+    :param max_tokens: Максимальное количество токенов в ответе.
+    :param yandex_api_key: API ключ для доступа к Yandex.
+    :param folder_id: ID папки в Yandex.
+    :return: Ответ от Yandex GPT или ошибка.
+    """
+    logger.debug("Начало генерации ответа от Yandex GPT.")
+    
+    def send_request_safety(payload: dict, api_key: str) -> tuple:
+        """
+        Отправка запроса в Yandex GPT с обработкой ошибок.
+
+        :param payload: Данные запроса.
+        :param api_key: API ключ для авторизации.
+        :return: Ответ от Yandex GPT или ошибка.
+        """
         url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {api_key}" if api_key.startswith('t1') else f"Api-Key {api_key}",
-            'x-folder-id': f"{folder_id}"
+            'x-folder-id': folder_id
         }
 
+        logger.debug(f"Отправка запроса на {url} с заголовками: {headers} и данными: {payload}")
+        
         response = requests.post(url, headers=headers, json=payload, timeout=180)
         if response.status_code == 200:
             response_json = response.json()
             text = response_json['result']['alternatives'][0]['message']['text']
+            logger.info("Запрос успешно выполнен, получен ответ от Yandex GPT.")
             return text, None
         else:
+            logger.error(f"Ошибка при выполнении запроса: {response.text}, статус код: {response.status_code}")
             return None, {
                 "error": {
                     "message": f"Ошибка: {response.text}",
@@ -26,23 +49,23 @@ def generate_yandexgpt_response(messages, model, temperature, max_tokens, yandex
                     "param": None,
                     "code": response.status_code
                 }
-            }  # Это вызовет исключение, если статус-код ответа не 200
+            }
     
-    # Формирование запроса в формате Yandex GPT
+    # Преобразование сообщений в формат Yandex GPT
     for message in messages:
-        message['text'] = message['content']
-        del message['content']
-        
-    if model == "gpt-4o":
-        model_uri = f"gpt://{folder_id}/yandexgpt/latest"
-    elif model == "gpt-4o-mini":
-        model_uri = f"gpt://{folder_id}/yandexgpt-lite/latest"
-    elif model.startswith("gpt://") or model.startswith("ds://"):
-        model_uri = model
-    else:
-        model_uri = f"gpt://{folder_id}/{model}"
+        message['text'] = message.pop('content', '')
+
+    # Определение URI модели
+    model_uri = (
+        f"gpt://{folder_id}/yandexgpt/latest" if model == "gpt-4o" else
+        f"gpt://{folder_id}/yandexgpt-lite/latest" if model == "gpt-4o-mini" else
+        model if model.startswith(("gpt://", "ds://")) else
+        f"gpt://{folder_id}/{model}"
+    )
     
-    # Формирование запроса в формате Yandex GPT
+    logger.debug(f"Определён URI модели: {model_uri}")
+    
+    # Формирование запроса
     payload = {
         "modelUri": model_uri,
         "completionOptions": {
@@ -53,48 +76,59 @@ def generate_yandexgpt_response(messages, model, temperature, max_tokens, yandex
         "messages": messages
     }
     
-    result = send_request_safety(payload, yandex_api_key)
-    
-    # Задержка от 429
-    # time.sleep(0.05)
-    
-    return result
+    logger.info("Формирование запроса завершено, отправка запроса в Yandex GPT.")
+    return send_request_safety(payload, yandex_api_key)
 
-# Генерация ответа от Yandex GPT
-def generate_yandex_embeddings_response(text, model, yandex_api_key, folder_id):
-    # Функция для отправки запроса в Yandex GPT
-    def send_request_safety(payload, api_key):
+def generate_yandex_embeddings_response(text: str, model: str, yandex_api_key: str, folder_id: str) -> list:
+    """
+    Генерация эмбеддингов с использованием Yandex GPT.
+
+    :param text: Текст для генерации эмбеддингов.
+    :param model: Модель для генерации эмбеддингов.
+    :param yandex_api_key: API ключ для авторизации.
+    :param folder_id: ID папки для Yandex API.
+    :return: Вектор эмбеддинга или сообщение об ошибке.
+    """
+    logger.debug("Начало генерации эмбеддингов.")
+    
+    def send_request_safety(payload: dict, api_key: str) -> list:
+        """
+        Отправка запроса в Yandex GPT с обработкой ошибок.
+
+        :param payload: Данные запроса.
+        :param api_key: API ключ для авторизации.
+        :return: Вектор эмбеддинга или сообщение об ошибке.
+        """
         url = "https://llm.api.cloud.yandex.net:443/foundationModels/v1/textEmbedding"
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {api_key}" if api_key.startswith('t1') else f"Api-Key {api_key}",
-            'x-folder-id': f"{folder_id}"
+            'x-folder-id': folder_id
         }
 
+        logger.debug(f"Отправка запроса на {url} с заголовками: {headers} и данными: {payload}")
+        
         response = requests.post(url, headers=headers, json=payload, timeout=180)
         if response.status_code == 200:
-            response_json = response.json()
-            vector = response_json["embedding"]
-            return vector
+            logger.info("Запрос успешно выполнен, получен ответ от Yandex GPT.")
+            return response.json().get("embedding", [])
         else:
-            return [f"Error: {response.status_code}, {response.text}"]
+            logger.error(f"Ошибка при выполнении запроса: {response.text}, статус код: {response.status_code}")
+            return [f"Ошибка: {response.status_code}, {response.text}"]
     
-    # Формирование запроса в формате Yandex GPT
-    if model == "text-embedding-3-large" or model == "text-embedding-3-small":
-        model_uri = f"emb://{folder_id}/text-search-doc/latest"
-    elif model.startswith("emb://") or model.startswith("ds://"):
-        model_uri = model
-    else:
-        model_uri = f"emb://{folder_id}/{model}"
+    # Формирование URI модели
+    model_uri = (
+        f"emb://{folder_id}/text-search-doc/latest" if model in ["text-embedding-3-large", "text-embedding-3-small"] else
+        model if model.startswith(("emb://", "ds://")) else
+        f"emb://{folder_id}/{model}"
+    )
+    
+    logger.debug(f"Определён URI модели: {model_uri}")
     
     payload = {
         "modelUri": model_uri,
         "text": text
     }
     
-    result = send_request_safety(payload, yandex_api_key)
-    
-    # Задержка от 429
-    # time.sleep(0.05)
-    
-    return result
+    logger.info("Формирование запроса завершено, отправка запроса в Yandex GPT.")
+    return send_request_safety(payload, yandex_api_key)
