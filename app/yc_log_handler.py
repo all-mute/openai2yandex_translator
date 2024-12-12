84eb289113525c5ae7faba30cd0b0c4a0a9ef64b
All Mute ✓ <davidmerkulov@yandex.ru>
2024-12-12 16:06:19 +0300

    Enhance logging and error handling in Yandex GPT integration
    - Added GITHUB_SHA to logger context for better traceability.
    - Improved logging structure in the completion endpoint with contextualized logging.
    - Implemented message obfuscation for sensitive information in logs.
    - Updated Yandex API key handling and error responses for robustness.
    - Minor adjustments in test configuration for local development.
    
    These changes improve the reliability and security of the Yandex integration, ensuring sensitive data is protected while enhancing the overall logging capabilities.

--- app/yc_log_handler.py
+++ app/yc_log_handler.py
@@ -1,8 +1,22 @@
 import logging
 from pythonjsonlogger import jsonlogger
+import dotenv
+import os
+import re
+
+dotenv.load_dotenv()
+
+YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
+
+def obfuscate_message(message: str):
+    """Obfuscate sensitive information."""
+    result = re.sub(r'(Api-Key|Bearer|OAuth|OPENAI_API_KEY:|Ключ:|ключ:) [A-Za-z0-9_\-@]+', "***API_KEY_OBFUSCATED***", message)
+    return result
 
 class YcLoggingFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
+        record.message = obfuscate_message(record.getMessage())
+        
         super(YcLoggingFormatter, self).add_fields(log_record, record, message_dict)
         log_record['logger'] = record.name
         log_record['level'] = str.replace(str.replace(record.levelname, "WARNING", "WARN"), "CRITICAL", "FATAL")
